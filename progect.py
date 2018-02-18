23f5fb81c44131b08e8350ab0d63e1a1448f7bc4
Boriclav <32224254+Boriclav@users.noreply.github.com>
2018-02-18 19:11:47 +0300

    Update progect.py

--- progect.py
+++ progect.py
@@ -1,13 +1,12 @@
+
 # Heir durfe wir ändern diese Mothode fur nuclear hochladenz
-k = 0
-l = 0
 s = 'gatta'
 t = 'gaattc' # We input 2  chains of nuclear
-print('input straffe')
+#print('input straffe')
 st = -2 #int(input())
-print('input winner')
+#print('input winner')
 wi = 2 #int(input())
-print('input einbishen')
+#print('input einbishen')
 ei = -1 #int(input())
 s = s.split()
 s = list(''.join(s))
@@ -15,6 +14,15 @@ t = t.split()
 t = list(''.join(t)) 
 ls = len(s)
 lt = len(t) # j ist Linie, i ist Kolonne
+aat = '0'
+aas = '0' 
+p = [[0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0]]
 A = [[0,0,0,0,0,0],
 	 [0,0,0,0,0,0],
 	 [0,0,0,0,0,0],
@@ -22,6 +30,7 @@ A = [[0,0,0,0,0,0],
 	 [0,0,0,0,0,0],
 	 [0,0,0,0,0,0],
 	 [0,0,0,0,0,0]]
+B = A
 for i in range(lt+1):
 	A[i][0] = i * st
 	#l +=1
@@ -31,18 +40,46 @@ for j in range(ls+1):
 	#k +=1
 #print(A)
 for j in range(1, ls+1):
-	k +=1
+	#k +=1
 	for i in range(1, lt+1):
-		l += 1
-		if s[j-1] == t[j-1]:
+		#l += 1
+		if s[j-1] == t[i-1]:
 			A[i][j] = max (A[i - 1][j] + st, A[i-1][j-1] + wi, A[i][j - 1] + st)
+			a1 = A[i - 1][j] + st
+			a2 = A[i-1][j-1] + wi
+			a3 = A[i][j - 1] + st
+			if a1 > a2 and a1 > a3:
+				p[i][j]= 'l'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #L - max from Left 
+			elif a2 > a1 and a2 > a3:
+				p[i][j]= 'd'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #D - max from Diagonal 
+			elif a3 > a1 and a3 > a2: 
+				p[i][j]= 'u'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #U - max from Up
+			elif a1 == a2 and a1 > a3: 
+				p[i][j]= 'ld'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+			elif a2 == a3 and a2 > a1:
+				p[i][j]= 'du'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+			else:
+				p[i][j]= 'ul'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
 		else:
 			A[i][j] = max (A[i - 1][j] + st, A[i-1][j-1] + ei, A[i][j - 1] + st)
+			a1 = A[i - 1][j] + st
+			a2 = A[i-1][j-1] + ei
+			a3 = A[i][j - 1] + st
+			if a1 > a2 and a1 > a3:
+				p[i][j]= 'l'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)#L - max from Left 
+			elif a2 > a1 and a2 > a3:
+				p[i][j]= 'd'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #D - max from Diagonal 
+			elif a3 > a1 and a3 > a2: 
+				p[i][j]= 'u'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #U - max from Up
+			elif a1 == a2 and a1 > a3: 
+				p[i][j]= 'ld'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+			elif a2 == a3 and a2 > a1:
+				p[i][j]= 'du'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+			else:
+				p[i][j]= 'ul'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
 		#print(A)
 for r in A:
 	print(r)
-
-#print(A[2][2])
-print('k=', k)
-print('l=', l)
-# j ist Linie, i ist Kolonne
+for g in p:
+	print(g)
+	
