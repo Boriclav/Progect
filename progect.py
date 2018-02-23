d116f5c797bef5be06dd0ca05f00ab8ad21edae5
Boriclav <32224254+Boriclav@users.noreply.github.com>
2018-02-23 18:43:43 +0300

    Update progect.py

--- progect.py
+++ progect.py
@@ -1,7 +1,8 @@
+# Heir durfe wir ändern diese Mothode fur nuclear hochladenz
 print ('Enter 1 nuclears code')
-s = input() #'gatta'
+s = 'gatta' #input()
 print ('Enter 2 nuclears code')
-t = input() #'gaattc' # We input 2  chains of nuclear
+t = 'gaattc'  #input() # We input 2  chains of nuclear
 #print('input straffe')
 st = -2 #int(input())
 #print('input winner')
@@ -16,8 +17,28 @@ ls = len(s)
 lt = len(t) # j ist Linie, i ist Kolonne
 aat = '0'
 aas = '0' 
+w = []
+Т1 = []  #for chain t
+S = []   #for chain s
+S1 = []  
 A = []
 P = []
+'''
+A = [[0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0]]
+P = [[0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0],
+	 [0,0,0,0,0,0]]
+'''
 for r in range(lt+1):
 	A.append([])
 	for c in range(ls+1):
@@ -26,71 +47,78 @@ for q in range(lt+1):
 	P.append([])
 	for d in range(ls+1):
 		P[q].append([0])
-'''	
-p = [[0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0]]
-A = [[0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0],
-	 [0,0,0,0,0,0]]
-''''
 for i in range(lt+1):
 	A[i][0] = i * st
-	#l +=1
-#print(A)
 for j in range(ls+1):
 	A[0][j] = j * st
-	#k +=1
-#print(A)
 for j in range(1, ls+1):
-	#k +=1
 	for i in range(1, lt+1):
-		#l += 1
 		if s[j-1] == t[i-1]:
-			A[i][j] = max (A[i - 1][j] + st, A[i-1][j-1] + wi, A[i][j - 1] + st)
 			a1 = A[i - 1][j] + st
 			a2 = A[i-1][j-1] + wi
 			a3 = A[i][j - 1] + st
+			A[i][j] = max (a1, a2, a3)
 			if a1 > a2 and a1 > a3:
-				p[i][j]= 'l'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #L - max from Left 
+				P[i][j]= 'l' #L - max from Left 
 			elif a2 > a1 and a2 > a3:
-				p[i][j]= 'd'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #D - max from Diagonal 
+				P[i][j]= 'd' #D - max from Diagonal 
 			elif a3 > a1 and a3 > a2: 
-				p[i][j]= 'u'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #U - max from Up
+				P[i][j]= 'u'#U - max from Up
 			elif a1 == a2 and a1 > a3: 
-				p[i][j]= 'ld'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+				P[i][j]= 'ld'
 			elif a2 == a3 and a2 > a1:
-				p[i][j]= 'du'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+				P[i][j]= 'du'
 			else:
-				p[i][j]= 'ul'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+				P[i][j]= 'ul'
 		else:
-			A[i][j] = max (A[i - 1][j] + st, A[i-1][j-1] + ei, A[i][j - 1] + st)
 			a1 = A[i - 1][j] + st
 			a2 = A[i-1][j-1] + ei
 			a3 = A[i][j - 1] + st
+			A[i][j] = max (a1, a2, a3)
 			if a1 > a2 and a1 > a3:
-				p[i][j]= 'l'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)#L - max from Left 
+				P[i][j]= 'l'#L - max from Left 
 			elif a2 > a1 and a2 > a3:
-				p[i][j]= 'd'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #D - max from Diagonal 
+				P[i][j]= 'd' #D - max from Diagonal 
 			elif a3 > a1 and a3 > a2: 
-				p[i][j]= 'u'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3) #U - max from Up
+				P[i][j]= 'u' #U - max from Up
 			elif a1 == a2 and a1 > a3: 
-				p[i][j]= 'ld'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+				P[i][j]= 'ld'
 			elif a2 == a3 and a2 > a1:
-				p[i][j]= 'du'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
+				P[i][j]= 'du'
 			else:
-				p[i][j]= 'ul'+ ' ' + str(a1) + ' '+ str(a2) + ' ' +str(a3)
-		#print(A)
+				P[i][j]= 'ul'
+'''
 for r in A:
 	print(r)
-for g in p:
+for g in P:
 	print(g)
-	
+'''
+i = lt
+j = ls
+while i!=0 and	j!=0:
+	#global T,S,T1,S1
+	if P[i][j] == 'd':
+		w.append(t[i-1])
+		S.append(s[j-1])
+		i -= 1
+		j -= 1
+	elif P[i][j] == 'u':
+		w.append('-')
+		S.append(s[j-1])
+		j -= 1
+	elif P[i][j] == 'l':
+		w.append(t[i-1])
+		S.append('-')
+		i -= 1
+	elif P[i][j] == 'du':
+		w.append(t[i-1])
+		S.append(s[j-1])
+		i -= 1
+		j -= 1
+	elif P[i][j] == 'ld':
+		w.append(t[i-1])
+		S.append(s[j-1])
+		i -= 1
+		j -= 1
+print('Chain 1:=', w[::-1])
+print('Chain 2:=', S[::-1])
